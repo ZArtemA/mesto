f5fd5ba64a2e845a4704189e11e3c1c135fbb617
Artem Zyrianov <mister.zyrianovartyom@yandex.ru>
2020-09-20 06:00:01 +0600

    Исправления 4 проектной работы

--- scripts/script.js
+++ scripts/script.js
@@ -3,32 +3,27 @@ let menu = page.querySelector('.popup');
 let edit = page.querySelector('.profile__btn-edit');
 let close = page.querySelector('.popup__btn-close');
 let save = page.querySelector('.popup__btn-save');
-
-
-function openClose() {
-    menu.classList.toggle('popup_opened');
-}
-
 let formElement = page.querySelector('.popup__textplace');
+let nameInput = page.querySelector('.popup__name');
+let jobInput = page.querySelector('.popup__profession');
+let name = page.querySelector('.profile__name');
+let profession = page.querySelector('.profile__profession');
+
+function togglePopup() {
+    menu.classList.toggle('popup_opened')
+        if (menu.classList.contains('popup_opened')) {
+            nameInput.value = name.textContent;
+            jobInput.value = profession.textContent;
+        }
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    let nameInput = page.querySelector('.popup__name');
-    let jobInput = page.querySelector('.popup__profession');
-
-    let newname = nameInput.value;
-    let newprofession = jobInput.value;
-
-    let name = page.querySelector('.profile__name');
-    let profession = page.querySelector('.profile__profession');
-
-    name.textContent = newname;
-    profession.textContent = newprofession;
-
+    name.textContent = nameInput.value;
+    profession.textContent = jobInput.value;
+    togglePopup()
 }
 
-edit.addEventListener('click', openClose);
-close.addEventListener('click', openClose);
-save.addEventListener('click', openClose);
+edit.addEventListener('click', togglePopup);
+close.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
