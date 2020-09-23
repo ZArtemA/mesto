54d77037ca635dce4977c6ccf08a48563479e348
Artem Zyrianov <mister.zyrianovartyom@yandex.ru>
2020-09-24 01:12:00 +0600

    Проектная работа 4 исправление JS и позиционирования блоков

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ let page = document.querySelector('.page');
 let menu = page.querySelector('.popup');
 let edit = page.querySelector('.profile__btn-edit');
 let close = page.querySelector('.popup__btn-close');
-let save = page.querySelector('.popup__btn-save');
 let formElement = page.querySelector('.popup__textplace');
 let nameInput = page.querySelector('.popup__name');
 let jobInput = page.querySelector('.popup__profession');
@@ -10,18 +9,18 @@ let name = page.querySelector('.profile__name');
 let profession = page.querySelector('.profile__profession');
 
 function togglePopup() {
-    menu.classList.toggle('popup_opened')
-        if (menu.classList.contains('popup_opened')) {
+        if (!menu.classList.contains('popup_opened')) {
             nameInput.value = name.textContent;
             jobInput.value = profession.textContent;
         }
+    menu.classList.toggle('popup_opened')
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     name.textContent = nameInput.value;
     profession.textContent = jobInput.value;
-    togglePopup()
+    togglePopup();
 }
 
 edit.addEventListener('click', togglePopup);
