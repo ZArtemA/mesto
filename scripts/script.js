b6f17c78e6a111529d8ec0d527e858562bdecad0
Artem Zyrianov <mister.zyrianovartyom@yandex.ru>
2020-11-02 03:01:26 +0600

    Контрольная 6 вынес отдельно массив

--- scripts/script.js
+++ scripts/script.js
@@ -22,32 +22,6 @@ const imageTitle = imageBig.querySelector('.popup__text');
 const imageLink = imageBig.querySelector('.popup__image-big');
 const escape = 'Escape';
 const cardTemplate = document.querySelector('#card').content;
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
 function renderCards(massive){
     massive.forEach(card => addToGallery(card.name, card.link));
 }
